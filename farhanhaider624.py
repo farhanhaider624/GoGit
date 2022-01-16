d58706c024d23a95e0c5efc0fa41c08dc4409dfd
farhanhaider624 <farhanhaider624@gmail.com>
2022-01-16 17:33:36 +0530

    commit 3

--- farhanhaider624.py
+++ farhanhaider624.py
@@ -1,2 +1,3 @@
 Money Heist
 I like this series because of the Professor (the master mind) who do the planning by considering every possible situation to come during the heist with their solution.
+NA
